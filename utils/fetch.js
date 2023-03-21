06e7d6c350e4951107088041963875499e653cf4
Nina Vens <nina.vens@hva.nl>
2023-03-21 16:04:47 +0100

    [added] api key

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,14 +1,14 @@
 import fetch from 'node-fetch';
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 // fetching data from the Rijkmuseum API based on user input
 const fetchData = async (userInput, resultCount) => {
 	let data;
-
-	const url = `https://www.rijksmuseum.nl/api/en/collection?key=RdKQCPfy&q=${userInput}&ps=${resultCount}&imgonly=true`;
+	const url = `https://www.rijksmuseum.nl/api/en/collection?key=${process.env.API_KEY}&q=${userInput}&ps=${resultCount}&imgonly=true`;
 
 	try {
 		data = await (await fetch(url)).json();
-
 		return data.artObjects;
 	} catch {
 		console.error('error');
@@ -18,13 +18,10 @@ const fetchData = async (userInput, resultCount) => {
 // fetching data from the Rijkmuseum API based on id
 const fetchDataIndividual = async (id) => {
 	let data;
-
-	const url = `https://www.rijksmuseum.nl/api/en/collection/${id}?key=RdKQCPfy`;
+	const url = `https://www.rijksmuseum.nl/api/en/collection/${id}?key=${process.env.API_KEY}`;
 
 	try {
 		data = await (await fetch(url)).json();
-
-        console.log(data.artObject)
 		return data.artObject;
 	} catch {
 		console.error('error');
