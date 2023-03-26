23ea37d992dae530bd93c75caca19aade3f22491
Nina Vens <nina.vens@hva.nl>
2023-03-26 16:38:32 +0200

    [fixed] resultCount cookie

--- routes/art.js
+++ routes/art.js
@@ -5,17 +5,22 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
 	let randomArt;
-    const resultCount = req.cookies.resultCount || req.query.resultCount;
-	res.cookie('resultCount', resultCount);
+	let resultCount = req.cookies.resultCount || req.query.resultCount;
 	const userInput = req.query.userInput;
+  
+	if (req.query.resultCount) {
+	  resultCount = req.query.resultCount;
+	  res.cookie('resultCount', resultCount);
+	}
+
 	const language = req.cookies.language || 'en';
 
 	const artItem = await fetchData(userInput, resultCount);
-	
+
 	if (req.body['fetchRandomArt']) {
 		randomArt = await fetchRandomArt();
 	}
-	
+
 	randomArt = await fetchRandomArt();
 
 	res.render('art', { css: ['views/normalView'], language, artItem, userInput, resultCount, randomArt });
