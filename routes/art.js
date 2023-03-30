053fa66a9049d8778d7ccbbfcbfd9e205fad3c1a
Nina Vens <nina.vens@hva.nl>
2023-03-30 14:43:52 +0200

    [added] prettier-ignore

--- routes/art.js
+++ routes/art.js
@@ -25,15 +25,11 @@ router.get('/', async (req, res) => {
 	}
 
 	randomArt = await fetchRandomArt();
-
+	// prettier-ignore
 	res.render('art', {
 		css: ['views/normalView'],
 		js: ['autocomplete/autocomplete', 'scrollIntoView'],
-		language,
-		artItem,
-		userInput,
-		resultCount,
-		randomArt
+		language, artItem, userInput, resultCount, randomArt
 	});
 });
 
