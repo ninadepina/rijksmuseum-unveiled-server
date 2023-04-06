8079b430a661a1c091f8d1c3fe0f52e4da482b12
Nina Vens <nina.vens@hva.nl>
2023-04-06 15:52:38 +0200

    [cleaned]

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,8 @@
 export default {
-    eq: function (a, b) {
-        return a === b;
-    },
-    sliceUrl: function (url) {
-        return (url || []).slice(0, -3);
-    }
+	eq: function (a, b) {
+		return a === b;
+	},
+	sliceUrl: function (url) {
+		return (url || []).slice(0, -3);
+	}
 };
