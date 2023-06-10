87e7891bac875a6855336c93488b59d256eb4c52
Nina Vens <nina.vens@hva.nl>
2023-06-10 15:43:26 +0200

    [added] random color art

--- routes/colorfilter.js
+++ routes/colorfilter.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import { fetchRandomArt, fetchColorData } from '../utils/fetch.js';
 const router = express.Router();
+// prettier-ignore
+const colorValues = ['000000', '737c84', 'b5bfcc', 'f6ecf3', 'dda5aa', 'f49b7a', 'df4c93', 'de4153', '981313', 'b35a1f', 'e09714', 'ffeb00', 'fbf6e1', 'e0cc91', '62ad77', '2f4f4f', '4279db', '8268dc', '4019b1', '850085'];
+
+const getRandomColor = () => {
+	const randomIndex = Math.floor(Math.random() * colorValues.length);
+	return colorValues[randomIndex];
+};
 
 router.get('/', async (req, res) => {
-	const color = req.query.color;
+	const color = req.query.color || getRandomColor();
 	const language = req.cookies.language || 'en';
 
 	const randomArt = await fetchRandomArt(language);
@@ -11,7 +18,10 @@ router.get('/', async (req, res) => {
 
 	res.render('colorfilter', {
 		css: ['views/colorFilterView', 'views/normalView'],
-		language, randomArt, colorData, href: '/colorfilter',
+		language,
+		randomArt,
+		colorData,
+		href: '/colorfilter'
 	});
 });
 
